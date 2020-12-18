64876cce29c60d5f0e971e271dc77d4e21fcfd3f
magikmaxx <mk.motschmann@gmail.com>
2020-12-18 10:06:27 +1000

    Commit (11)

--- breakdown_of_income_expenses.rb
+++ breakdown_of_income_expenses.rb
@@ -12,14 +12,14 @@ def breakdown_of_income_expenses_state
     categories_hash.each do |k, v|
         $new_balance = balance - (categories_hash.values.sum)
     end
-    
+
     if balance > 0 
-        puts "Your balance           #{balance}".colorize(:green)
+        puts "Your balance:          #{balance}".colorize(:green)
     else
-        puts "Your balance           #{balance}".colorize(:red)
+        puts "Your balance:          #{balance}".colorize(:red)
     end    
-    categories_hash.each do |k, v|
-        puts "#{k}              #{v}".colorize(:red)
+    categories_hash.each do |category_key, category_value|
+        puts "#{category_key}              #{category_value}".colorize(:red)
         end 
     if $new_balance > 0
         puts "Your new balance:      #{$new_balance}".colorize(:green)
