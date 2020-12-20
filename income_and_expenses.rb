84c3202e04d10bf7c317d2a9a7fddfe670f3f916
magikmaxx <mk.motschmann@gmail.com>
2020-12-20 12:16:21 +1000

    Income and Expenses edit

--- income_and_expenses.rb
+++ income_and_expenses.rb
@@ -9,7 +9,7 @@ def expenses
     category = categories.find { |category| category == chosen_category }
     if category
       while expense_amount.nil? do
-      puts "What amount?".colorize(:blue)
+      puts "What amount?".colorize(:light_blue)
       expense_amount = Integer(gets.chomp) rescue nil
       end
       @category_hash[category] += expense_amount
@@ -18,15 +18,15 @@ def expenses
       puts "Category does not exist".colorize(:cyan)
       return expenses
     end
-    puts "#{expense_amount} has been added to #{chosen_category}"
+    puts "#{expense_amount} has been added to #{chosen_category}".colorize(:green)
   end
 
 def income
     input_balance = nil
     while input_balance.nil? do
-    puts "Please enter your income".colorize(:blue)
+    puts "Please enter your income".colorize(:light_blue)
     input_balance = Integer(gets.chomp) rescue nil
     end
     @balance += input_balance
-    puts "Your added income is #{@balance}".colorize(:blue)
+    puts "Your added income is #{@balance}".colorize(:green)
 end
